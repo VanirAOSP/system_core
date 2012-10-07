LineageOS/android_system_core_old

storage: a more flexible fuse daemon
Needed for: http://review.cyanogenmod.com/#/c/20696/

Change-Id: Iaace4e125f2731f7fe50273d19af918d8f31989f

storage/Android.mk
@@ -1,0 +1,27 @@
+#
+#  Copyright (C) 2010 The Android Open Source Project
+#  Copyright (C) 2012 The CyanogenMod Project
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := storage.c
+LOCAL_MODULE := storage
+LOCAL_MODULE_TAGS := user
+
+LOCAL_SHARED_LIBRARIES := libc
+
+include $(BUILD_EXECUTABLE)

VanirAOSP/system_core

storage/Android.mk
@@ -1,0 +1,27 @@
+#
+#  Copyright (C) 2010 The Android Open Source Project
+#  Copyright (C) 2012 The CyanogenMod Project
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := storage.c
+LOCAL_MODULE := storage
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SHARED_LIBRARIES := libc
+
+include $(BUILD_EXECUTABLE)
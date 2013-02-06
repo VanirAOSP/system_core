aaace8f4135681cc8490ea2d7244cc0e599e71b0
Noah Greene <chevycam94@gmail.com>
2013-02-06 15:28:32 -0500

    Remove selinux from repo
    Somehow this stuff slipped in from master branch
    
    Change-Id: I92b66889cbd569ccf259336648752d9fff7faa8f

--- init/Android.mk
+++ init/Android.mk
@@ -55,12 +55,6 @@ LOCAL_UNSTRIPPED_PATH := $(TARGET_ROOT_OUT_UNSTRIPPED)
 
 LOCAL_STATIC_LIBRARIES := libfs_mgr libcutils libc
 
-ifeq ($(HAVE_SELINUX),true)
-LOCAL_STATIC_LIBRARIES += libselinux
-LOCAL_C_INCLUDES += external/libselinux/include
-LOCAL_CFLAGS += -DHAVE_SELINUX
-endif
-
 include $(BUILD_EXECUTABLE)
 
 # Make a symlink from /sbin/ueventd and /sbin/watchdogd to /init

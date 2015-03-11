de1cb4b6cfa67c55a72d39d517ea8429ae319093
nuclearmistake <nuclearmistake@gmail.com>
2015-03-10 21:53:08 -0400

    Fix-up healthd_mode_battery.cpp
    Change-Id: Ib785d11e135654bc906dec86ee755383e232e104

--- healthd/healthd_mode_charger.cpp
+++ healthd/healthd_mode_charger.cpp
@@ -90,6 +90,7 @@ char *locale;
 
 #define LOGE(x...) do { KLOG_ERROR("charger", x); } while (0)
 #define LOGW(x...) do { KLOG_WARNING("charger", x); } while (0)
+#define LOGI(x...) do { KLOG_INFO("charger", x); } while (0)
 #define LOGV(x...) do { KLOG_DEBUG("charger", x); } while (0)
 
 extern int mode;
@@ -766,10 +767,12 @@ static void handle_power_supply_state(struct charger *charger, int64_t now)
     if (!charger->have_battery_state)
         return;
 
-    soc = get_battery_capacity();
-    if (old_soc != soc) {
-        old_soc = soc;
-        set_battery_soc_leds(soc);
+    if (batt_prop) {
+        soc = batt_prop->batteryLevel();
+        if (old_soc != soc) {
+            old_soc = soc;
+            set_battery_soc_leds(soc);
+        }
     }
 
     if (!charger->charger_connected) {
